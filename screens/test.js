9e195884696ac65559b827148ac30f2b28ef546c
ItsThompson <33135895+ItsThompson@users.noreply.github.com>
2020-11-05 20:03:19 +0800

    🎉 Database content finally connected to react native

--- screens/test.js
+++ screens/test.js
@@ -1,7 +1,46 @@
-import React from "react";
-import { StyleSheet, Dimensions, Text} from "react-native";
-import { FlatList } from "react-native-gesture-handler";
-import {decode, encode} from 'base-64'
+// import React, { useEffect, useState } from "react";
+// import { ActivityIndicator, FlatList, Text, View } from "react-native";
+
+// // https://reactnative.dev/docs/network
+// export default App = () => {
+//     const [isLoading, setLoading] = useState(true);
+//     const [data, setData] = useState([]);
+
+//     useEffect(() => {
+//         fetch(
+//             "https://jsonkeeper.com/b/B5UY"
+//         )
+//             .then((response) => response.json())
+//             .then((json) => setData(json.credits))
+//             .catch((error) => console.error(error))
+//             .finally(() => setLoading(false));
+//     }, []);
+
+//     console.log(data)
+
+//     return (
+//         <View style={{ flex: 1, padding: 24 }}>
+//             {isLoading ? (
+//                 <ActivityIndicator />
+//             ) : (
+//                 // https://reactnative.dev/docs/flatlist.html
+//                 <FlatList
+//                     data={data}
+//                     keyExtractor={({ id }) => id}
+//                     renderItem={({ item }) => (
+//                         <Text>
+//                             {item.name}, {item.role}
+//                         </Text>
+//                     )}
+//                 />
+//             )}
+//         </View>
+//     );
+// };
+
+import React, { useEffect, useState } from "react";
+import { Text, FlatList, ActivityIndicator, View } from "react-native";
+import {decode, encode} from 'base-64';
 
 const textsByLanguageAndTitle = require("../services/textsByLanguagesAndTitle")
 
@@ -9,37 +48,58 @@ if (!global.btoa) {  global.btoa = encode }
 
 if (!global.atob) { global.atob = decode }
 
-const width = Dimensions.get("window").width;
-const height = Dimensions.get("window").height;
-
-function Info(){
-    return new Promise((resolve) => {
-        textsByLanguageAndTitle("english", "test").then((data) => {
-            console.log(data);
-            let test = data[0];
-            resolve(test);
-            return;
-        })
-    })
-}
+// const width = Dimensions.get("window").width;
+// const height = Dimensions.get("window").height;
 
 export default function App() {
-    const test = Info();
+    const [isLoading, setLoading] = useState(true);
+    const [data, setData] = useState([]);
+    
+    useEffect(() => {
+        textsByLanguageAndTitle("english", "test")
+            .then((output) => {
+                console.log('test.js');
+                console.log(output);
+                let out = output;
+                setData(out);
+            })
+            .catch((error) => {
+                console.error(error);
+            })
+            .finally(() => {
+                setLoading(false);
+            });
+    }, [])
+
     return(
-        <Text>{test}</Text>
+        <View style={{ flex: 1, padding: 24 }}>
+            {
+                isLoading ? (
+                    <ActivityIndicator/>
+                ) : (
+                    <FlatList
+                        data={data}
+                        renderItem={({ item }) => (
+                            <Text>{item.data}</Text>
+                        )}
+                        keyExtractor = {item => item.id}
+                    />
+                )
+            }
+        </View>
     )
 }
-const styles = StyleSheet.create({
-    image: {
-        padding: 0,
-        resizeMode: "contain",
-        height: height * 0.4,
-        width: width * 0.8,
-    },
-    text: {
-        fontSize: 18,
-        color: "black",
-        fontWeight: "bold",
-        alignSelf: "center",
-    }
-})
+// // Old Code just incase i need it
+//     // // const test = Info();
+//     // // let data = data[0];
+//     // // console.log(typeof foo);
+//     // // Proof of concept
+//     // const Test = () => {
+//     //     return <Text>test</Text>
+//     // };
+//     // // Error: Objects are not valid as React child (found: object with keys {}).
+//     // // If you meant to render a collection of children, use an array instead.
+
+//     // return(
+//     //     <Test/>
+//    // )
--- services/textsByLanguagesAndTitle.js
+++ services/textsByLanguagesAndTitle.js
@@ -2,6 +2,7 @@ const faunadb = require('faunadb');
 const client = new faunadb.Client({secret : 'fnAD5UwXOJACDCQF7orPKwdZsb2gOr46bgD3Pp_l'});
 
 const {
+    Get,
     Paginate,
     Match,
     Index,
@@ -12,12 +13,27 @@ const textsByLanguageAndTitle = async(language, title) => {
     const doc = await client.query(
         Paginate(
             Match(
-                Index('texts_by_language_and_title'), language, title
+                Index('test'), language, title
             )
         )
+        // Paginate(
+        //     Match(
+        //         Index('texts_by_language_and_title'), language, title
+        //     )
+        // )
     )
     .catch((error) => console.error(error));
-    return doc["data"]
+    let out = [
+        {
+            id: "0",
+            data: doc["data"][0],
+        },
+    ];
+    console.log('----------------------------------')
+    console.log('services etc etc');
+    console.log(out);
+    // console.log(doc)
+    return out;
 }
 
 // textsByLanguageAndTitle("english", "test").then(data => {

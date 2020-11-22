ab59398700b64aa16b2b9260403f00a94c133b3f
ItsThompson <33135895+ItsThompson@users.noreply.github.com>
2020-11-22 18:27:27 +0800

    Added a comment

--- components/questions.js
+++ components/questions.js
@@ -1,6 +1,8 @@
 import React from "react";
 import { View, Text } from "react-native";
 
+// This is a component of the quiz.js script.
+
 export default class CustomText extends React.Component { 
     render() { 
         return (
